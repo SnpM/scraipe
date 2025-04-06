c742fd2ce4cc52b2e8562a62b8b46cb9240cb317
nibs <jpthek9@gmail.com>
2025-04-06 17:55:45 -0400

    Remove llm_analyzer_usage.py; Add GeminiAnalyzer example

--- examples/gemini_analyzer_example.py
+++ examples/gemini_analyzer_example.py
@@ -1,10 +1,9 @@
 import os
-from scraipe.extended.llm_analyzers import OpenAiAnalyzer
+from scraipe.extended.llm_analyzers import GeminiAnalyzer
 from pydantic import BaseModel
-
-# Load OpenAI API key from environment variable
-# Set this in bash using `export OPENAI_API_KEY=example-key`
-openai_key = os.getenv("OPENAI_API_KEY")
+# Load API key from environment variable
+# Set this in bash using `export GEMINI_API_KEY=example-key`
+gemini_key = os.getenv("GEMINI_API_KEY")
 
 # Craft an instruction for the analyzer that specifies the information
 # you want to extract and the JSON schema to format the output as
@@ -16,15 +15,13 @@ Output the result in the following JSON format:
     "sentiment": "<positive|negative|neutral>",
 }
 """
-
 # Define a pydantic schema for the expected output
 class ExpectedOutput(BaseModel):
     topic: str
     sentiment: str
-
-# Initialize the OpenAiAnalyzer with the API key, instruction, and schema
-analyzer = OpenAiAnalyzer(
-    api_key=openai_key,
+# Initialize the analyzer with the API key, instruction, and schema
+analyzer = GeminiAnalyzer(
+    api_key=gemini_key,
     instruction=instruction,
     pydantic_schema=ExpectedOutput,
 )
@@ -35,6 +32,5 @@ Scraipe is a powerful tool for scraping and analyzing web data. It allows users 
 With Scraipe, users can automate the process of data collection and analysis, saving time and effort.
 """
 result = analyzer.analyze(article)
-
 # Output the analysis result
 print(result.output)
