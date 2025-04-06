01091c55d970703cfddff44c34697c69c4a03cc0
nibs <jpthek9@gmail.com>
2025-04-06 18:20:58 -0400

    Tweak comments

--- examples/gemini_analyzer_example.py
+++ examples/gemini_analyzer_example.py
@@ -1,12 +1,11 @@
 import os
 from scraipe.extended.llm_analyzers import GeminiAnalyzer
 from pydantic import BaseModel
-# Load API key from environment variable
-# Set this in bash using `export GEMINI_API_KEY=example-key`
+
+# Set using `export GEMINI_API_KEY=your-api-key`
 gemini_key = os.getenv("GEMINI_API_KEY")
 
-# Craft an instruction for the analyzer that specifies the information
-# you want to extract and the JSON schema to format the output as
+# Craft an instruction for the LLM
 instruction = """
 Determine the article's topic and whether the sentiment is positive, negative, or neutral.
 Output the result in the following JSON format:
@@ -15,10 +14,12 @@ Output the result in the following JSON format:
     "sentiment": "<positive|negative|neutral>",
 }
 """
+
 # Define a pydantic schema for the expected output
 class ExpectedOutput(BaseModel):
     topic: str
     sentiment: str
+    
 # Initialize the analyzer with the API key, instruction, and schema
 analyzer = GeminiAnalyzer(
     api_key=gemini_key,
@@ -32,5 +33,6 @@ Scraipe is a powerful tool for scraping and analyzing web data. It allows users 
 With Scraipe, users can automate the process of data collection and analysis, saving time and effort.
 """
 result = analyzer.analyze(article)
+
 # Output the analysis result
 print(result.output)
